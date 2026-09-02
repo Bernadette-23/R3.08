9d199c5eed38a28c02fd4a23583bfffbcb2b43af
Bernadette-23 <lafagebernadette23@gmail.com>
2026-09-02 15:01:43 +0200

    maj

--- fonction2.py
+++ fonction2.py
@@ -1,9 +1,10 @@
-n1 = int(input("Entrez le nombre 1 : "))
-
 def seuil (n1 : int, seuil =10):
     if n1 > seuil:
         return f"Le nombre {n1} est supérieur au seuil de {seuil}"
     else:
         return f"le nombre saisi est {n1}"
 
-print(seuil(n1))
+
+if __name__ == "__main__":
+    n1 = int(input("Entrez le nombre 1 : "))
+    print(seuil(n1))
