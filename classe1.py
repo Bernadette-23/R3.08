47bd5892db19af1105212171fba880adb3693217
Bernadette-23 <lafagebernadette23@gmail.com>
2026-09-08 16:17:58 +0200

    maj

---
+++ classe1.py
@@ -0,0 +1,28 @@
+class Tasse:
+
+    matiere : str = "céramique"
+
+    def __init__(self, couleur, contenance, marque):
+        self.couleur = couleur
+        self.contenance = contenance
+        self.marque = marque
+
+    def __str__(self):
+        return f"La tasse de matiere {Tasse.matiere}, de couleur {self.couleur} et de marque {self.marque} a une contenance de {self.contenance} ml"
+
+    def contenu(self,contenu):
+        self.contenu = contenu
+
+    def bue(self):
+        del(self.contenu)
+
+if __name__ == "__main__":
+    mc = Tasse("bleu","50","duralex")
+    print(mc)
+    print(vars(Tasse))
+    print(vars(mc))
+    mc.contenu("eau")
+    print(mc.contenu)
+    print(vars(mc))
+    mc.bue()
+    print(vars(mc))
