72ca555293b51a995d38a14afea0e101b17174db
Bernadette-23 <lafagebernadette23@gmail.com>
2026-09-11 09:26:53 +0200

    maj tp1

--- TP1/point.py
+++ TP1/point.py
@@ -10,9 +10,10 @@ class Point:
         return f"Piont : [{self.__x} ; {self.__y}]"
 
     def distanceCoordonnee(self, x:float,y:float):
-        distance = (math.sqrt((self.__x-x)**2 + (self.__y-y)**2)) #cette ligne peut etre remplacer par les 2 prochaines lignes
-                  #  (math.pow(self.__x-x,2) + math.pow(self.__y-y,2))
-                  #  ((self.__x-x)*(self.__x-x) + (self.__y-y)*(self.__y-y))
+        distance = (math.sqrt((self.__x-x)**2 + (self.__y-y)**2)) # cette ligne peut etre remplacer par les 2 prochaines lignes
+
+                  # 1ère option --> (math.pow(self.__x-x,2) + math.pow(self.__y-y,2))
+                  # 2eme option --> ((self.__x-x)*(self.__x-x) + (self.__y-y)*(self.__y-y))
         return distance
 
 if __name__ == "__main__":
