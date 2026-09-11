27d554a463058593ff40f004dd956b34b513df0b
Bernadette-23 <lafagebernadette23@gmail.com>
2026-09-11 09:25:26 +0200

    maj tp1

--- TP1/point.py
+++ TP1/point.py
@@ -1,6 +1,7 @@
+import math
+
 class Point:
-  #  x : float=0
-   # y : float=0
+
     def __init__(self, x : int=0, y : int=0):
         self.__x = x
         self.__y = y
@@ -8,9 +9,16 @@ class Point:
     def __str__(self):
         return f"Piont : [{self.__x} ; {self.__y}]"
 
+    def distanceCoordonnee(self, x:float,y:float):
+        distance = (math.sqrt((self.__x-x)**2 + (self.__y-y)**2)) #cette ligne peut etre remplacer par les 2 prochaines lignes
+                  #  (math.pow(self.__x-x,2) + math.pow(self.__y-y,2))
+                  #  ((self.__x-x)*(self.__x-x) + (self.__y-y)*(self.__y-y))
+        return distance
+
 if __name__ == "__main__":
     p1 = Point(3.2, 1)
     print (p1)
     p2 = Point()
     print (p2)
+    print(p1.distanceCoordonnee(0,0))
 
