8e52184830bf202457badc7f8c473e413b16b1f8
Bernadette-23 <lafagebernadette23@gmail.com>
2026-09-18 09:59:11 +0200

    maj

--- TP2/mmorpg.py
+++ TP2/mmorpg.py
@@ -6,6 +6,7 @@ class Personnage:
         self.__pv = n
         self.__initiative = n
 
+
     def attaquer(self, opposant : Personnage):
 
         if self.__initiative > opposant.__initiative:
@@ -30,15 +31,29 @@ class Personnage:
     def soigner(self):
          self.__pv =  self.__niveau
 
+class Guerrier(Personnage):
+
+    def __init__(self, pseudo : str, n: int = 1):
+        super().__init__(pseudo, n)
+        self.set_pv(n * 8 +4)
+        self.set_initiative(n *4 +6)
 
 
 
+class Mage(Personnage):
 
-
-
+    def __init__(self, pseudo: str, n: int = 1):
+        super().__init__(pseudo, n)
+        self.set_pv(n *5 +10)
+        self.set_initiative(n * 6+4)
+        self.__mana = n*5
 
 
 
 
 
 if __name__ == '__main__':
+    gerrier1 = Guerrier('Gerrier1', 2)
+    mage1 = Mage('Mage1', 3)
+    gerrier1.attaquer(mage1)
+    mage1.combat(gerrier1)
